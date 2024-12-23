76d7b12748ea11d4835a6e55e15ed741e235f335
akakj <iamannakand@gmail.com>
2024-12-23 15:47:02 +0000

    Nav bar fixed for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
           <img
             src={toggle ? close : menu}
             alt="menu"
-            className="w-[28px] h-[28px] object-contain"
+            className="w-[100px] h-[28px] object-contain"
             onClick={() => setToggle(!toggle)}
           />
 

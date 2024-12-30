61a318f4f6d53f72d0b0e0da3152c602033400db
akakj <iamannakand@gmail.com>
2024-12-30 00:45:20 +0000

    Alert added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
-
 import BackgroundMusic from './components/BackgroundMusic';
-
-import {
-  About,
-  Contact,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  Certifications,
-  StarsCanvas,
-} from "./components";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { About, Contact, Hero, Navbar, Tech, Works, Certifications, StarsCanvas } from "./components";
 
 const App = () => {
+  
+  useEffect(() => {
+    const originalConsoleWarn = console.warn;
+    console.warn = (message, ...args) => {
+      if (message.includes("Too many active WebGL contexts")) {
+        alert("WARNING: Due to your device/browser limited capabilities, some textures may be displayed incorrectly.");
+      }
+      originalConsoleWarn(message, ...args);
+    };
+
+    return () => {
+      console.warn = originalConsoleWarn;
+    };
+  }, []);
+  
   return (
     <BrowserRouter>
-    <ErrorBoundary>
       <div className="relative z-0 bg-primary">
         <BackgroundMusic />
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
@@ -26,14 +29,13 @@ const App = () => {
         </div>
         <About />
         <Tech />
-        <Certifications/>
+        <Certifications />
         <Works />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
-    </ErrorBoundary>
     </BrowserRouter>
   );
 };
